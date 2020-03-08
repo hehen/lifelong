43e54ccb47439848d9d52d77b61b1eb0f0dd4ae0
hehen <1124571759@qq.com>
2020-03-08 17:36:46 +0800

    session添加

--- src/main/java/com/cit/lifelong/controller/AuthorizeController.java
+++ src/main/java/com/cit/lifelong/controller/AuthorizeController.java
@@ -4,9 +4,12 @@ import com.cit.lifelong.dto.AccessTokenDTO;
 import com.cit.lifelong.dto.UserDTO;
 import com.cit.lifelong.provider.AuthorizeProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @ClassName AuthorizeController
@@ -20,18 +23,34 @@ public class AuthorizeController {
     @Autowired
     AuthorizeProvider authorizeProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.client.uri}")
+    private String callbackUri;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code
-            , @RequestParam(name = "state") String state) {
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest httpServletRequest) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("a0e7e5891c4c7ef54bba");
-        accessTokenDTO.setClient_secret("5ee12d0bea2911e801dd63e1c5534840df3c17d2");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8087/callback");
+        accessTokenDTO.setRedirect_uri(callbackUri);
         accessTokenDTO.setState(state);
         String accessToken = authorizeProvider.getAccessToken(accessTokenDTO);
         UserDTO userDTO = authorizeProvider.getUser(accessToken);
-        System.out.println("username = " + userDTO.getName());
-        return "index";
+        if(userDTO!=null){
+            System.out.println("username = " + userDTO.getName());
+            //登陆成功 写cookies和session
+            //TODO 为什么不是httpsession
+            httpServletRequest.getSession().setAttribute("user",userDTO);
+        } //登陆失败 重新登陆
+
+        return "redirect:/";
     }
 }
