e128512308c2fce043a07fa4ce74ea8659aeb816
hehen <1124571759@qq.com>
2020-03-10 21:55:39 +0800

    加入mybatis 插入github user到h2

---
+++ src/main/java/com/cit/lifelong/mapper/UserMapper.java
@@ -0,0 +1,4 @@
+package com.cit.lifelong.mapper;
+
+public interface UserMapper {
+}
---
+++ src/main/java/com/cit/lifelong/model/User.java
@@ -0,0 +1,11 @@
+package com.cit.lifelong.model;
+
+/**
+ * @ClassName User
+ * @Description: TODO
+ * @Author hehen
+ * @Date 2020/3/10
+ * @Version V1.0
+ **/
+public class User {
+}
